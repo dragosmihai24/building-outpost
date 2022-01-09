feb460bd7942abc2167944ec8c8dbff54a4b205b
Dragos Mihai <mihaimariandragos@gmail.com>
2022-01-09 18:32:36 +0200

    8oik9k9io

--- scriptsjs.js
+++ scriptsjs.js
@@ -1,6 +1,6 @@
 window.onload=()=>{
 var canvas = document.getElementById('can');
-var context=canvas.getContext('2d');
+var context = canvas.getContext('2d');
 var video = document.getElementById('video');
 var width = 350;
 var height = 200;
@@ -20,10 +20,60 @@ function render(){
     setTimeout(() => {
         render();
     }, 0);
+
 }
 
 img = document.getElementById('frame');
 img.addEventListener("click",render);
+}
+
+function alterId(id, x){
+    var altered = id;
+    altered = altered.slice(0, -1);
+    altered += x;
+    return altered;
+}
+
+function alive(id){
+
+    document.getElementById(id).style.opacity = 1;
+    document.getElementById(alterId(id, 's')).style.opacity = 0;
+    document.getElementById(alterId(id, 'a')).play();   
+}
+
+function dead(id){
+    document.getElementById(id).style.opacity = 0;
+    document.getElementById(alterId(id, 's')).style.opacity = 1;
+    document.getElementById(alterId(id, 'a')).pause();
+    document.getElementById(alterId(id, 'a')).currentTime = 0;
+}
+
+function colorFilter(id){ 
+    document.getElementById('can').style.filter = 'grayscale(100%)';
+
+}
+
+function removeFilter(id){
+    document.getElementById('can').style.filter = "none";
+    
+}
+
+function fadeMessage(){
+    document.getElementById('message').className = 'hidden';
+}
+
+//Geolocation
+function getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else{
+        alert("Please enable location on your browser.");
+    }
+}
+
+function showPosition(position){
+    alert("Latitude: " + position.coords.latitude + "\n" + "Longitude: " + position.coords.longitude);
 }
 
 
